9494944eecc4528c536e13565d3bf1cf0abd3021
Bryan Lim <bryan.l@digipen.edu.sg>
2026-07-10 02:35:27 +0800

    Fix Assignment 1 WebGL shader rendering

--- Assignment1/AndroidProgrammingQuiz-CrossPlatformCI/index.js
+++ Assignment1/AndroidProgrammingQuiz-CrossPlatformCI/index.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\bryan\AppData\Local\Temp\tmpm27minqc.js
+// include: C:\Users\bryan\AppData\Local\Temp\tmprhkf6vvs.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -200,21 +200,21 @@ Module['FS_createPath']("/assets", "shader", true, true);
 
   })();
 
-// end include: C:\Users\bryan\AppData\Local\Temp\tmpm27minqc.js
-// include: C:\Users\bryan\AppData\Local\Temp\tmpop3_luia.js
+// end include: C:\Users\bryan\AppData\Local\Temp\tmprhkf6vvs.js
+// include: C:\Users\bryan\AppData\Local\Temp\tmp2m4vjdu8.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\bryan\AppData\Local\Temp\tmpop3_luia.js
-// include: C:\Users\bryan\AppData\Local\Temp\tmpd49t5ssq.js
+  // end include: C:\Users\bryan\AppData\Local\Temp\tmp2m4vjdu8.js
+// include: C:\Users\bryan\AppData\Local\Temp\tmpc_32_evh.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\bryan\AppData\Local\Temp\tmpd49t5ssq.js
+  // end include: C:\Users\bryan\AppData\Local\Temp\tmpc_32_evh.js
 
 
 var programArgs = [];
